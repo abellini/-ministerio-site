97f92a523fc9f3c9b08885095ab44d12e3ac69e5
abellini <alexandrebli@gmail.com>
2018-12-02 00:03:12 -0200

    adicao missao de natal 2018

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,6 +9,15 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
     
   public pageTitle = 'Ministério Eucarístico';
+  public howare = 'Quem somos';
+  public wakeup = 'Despertai';
+  public voe = 'VOE';
+  public pleno = 'Pleno';
+  public kids = 'Kids';
+  public ide = 'Kids';
+  public comunite = 'Comunidade';
+  public depo = 'Depoimentos';
+  public loja = 'Loja Virtual';
 
   constructor() {
   }
